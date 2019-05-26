a0db96669cf4cbeffd6e8f35b93e3d286093b4c4
PeterBrw <peter.giurgiu@gmail.com>
2019-05-26 04:21:41 +0300

    adding-next-to-middlewares

--- controllers/notes.js
+++ controllers/notes.js
@@ -14,16 +14,16 @@ const footer = require('../views/includes/footer').getFooter();
 const headerSecond = require('../views/includes/header-second').headerSecond();
 const footerSecond = require('../views/includes/footer-second').footerSecond();
 
-exports.getHome = (req, res) => res.send(getHomePage(begin, between, headerMain, footer, end));
+exports.getHome = (req, res, next) => res.send(getHomePage(begin, between, headerMain, footer, end));
 
-exports.getNotes = (req, res) => res.send(getNotes(begin, between, footer, end));
+exports.getNotes = (req, res, next) => res.send(getNotes(begin, between, footer, end));
 
-exports.getLogin = (req, res) => res.send(getLogin(begin, between, headerSecond, footerSecond, end));
+exports.getLogin = (req, res, next) => res.send(getLogin(begin, between, headerSecond, footerSecond, end));
 
-exports.getLoginErr = (req, res) => res.send(getLoginErr(begin, between, headerSecond, footerSecond, end));
+exports.getLoginErr = (req, res, next) => res.send(getLoginErr(begin, between, headerSecond, footerSecond, end));
 
-exports.getSignup = (req, res) => res.send(getSignup(begin, between, headerSecond, footerSecond, end));
+exports.getSignup = (req, res, next) => res.send(getSignup(begin, between, headerSecond, footerSecond, end));
 
-exports.getSignupErr = (req, res) => res.send(getSignupErr(begin, between, headerSecond, footerSecond, end));
+exports.getSignupErr = (req, res, next) => res.send(getSignupErr(begin, between, headerSecond, footerSecond, end));
 
-exports.get404 = (req, res) => res.send(get404(begin, between, headerMain, footer, end));
+exports.get404 = (req, res, next) => res.send(get404(begin, between, headerMain, footer, end));
